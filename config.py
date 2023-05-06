ce22b3b230f7c88b0e6632acc0f7aff4a6e91059
EgorKhabarov <117845967+EgorKhabarov@users.noreply.github.com>
2023-05-07 01:33:00 +0400

    v07.05.2023

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ hours_difference = 4  # Разница времени для логов (на х
 bot_token = "https://t.me/BotFather"
 weather_api_key = "https://home.openweathermap.org/users/sign_in"
 
-__version__ = "2.0 16.03.2023"
+__version__ = "2.0 07.05.2023"
 __autor__ = "EgorKhabarov"
 
 """
