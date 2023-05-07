54da455244a881b7a37d7e6a5230c62a923c8f57
EgorKhabarov <117845967+EgorKhabarov@users.noreply.github.com>
2023-05-08 01:10:11 +0400

    v08.05.2023

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ hours_difference = 4  # Разница времени для логов (на х
 bot_token = "https://t.me/BotFather"
 weather_api_key = "https://home.openweathermap.org/users/sign_in"
 
-__version__ = "2.0 07.05.2023"
+__version__ = "08.05.2023"
 __autor__ = "EgorKhabarov"
 
 """
@@ -17,10 +17,11 @@ start - Start
 calendar - Calendar
 today - Today's message
 weather - {city} Weather
+forecast - {city} Weather forecast for 5 days
 week_event_list - Weekly events
-dice - Roll the dice
+dice - Roll the dice (randomizer)
+account - Account
 save_to_csv - Save my data in csv
 help - Help
 settings - Settings
 """
-
