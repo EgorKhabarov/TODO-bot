7e73a60945263fff5baad6ffc52db31072f11204
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2023-05-08 04:20:33 +0400

    v08.05.2023

--- config.py
+++ config.py
@@ -11,17 +11,4 @@ __autor__ = "EgorKhabarov"
 """
 Bot settings => Group Privacy => disabled
 Bot settings => Inline Mode   => disabled
-Edit Bot => Edit Commands: 
-
-start - Start
-calendar - Calendar
-today - Today's message
-weather - {city} Weather
-forecast - {city} Weather forecast for 5 days
-week_event_list - Weekly events
-dice - Roll the dice (randomizer)
-account - Account
-save_to_csv - Save my data in csv
-help - Help
-settings - Settings
 """
