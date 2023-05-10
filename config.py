ce035b08c782aef07cf7fe8798affa52da2ec7c2
EgorKhabarov <117845967+EgorKhabarov@users.noreply.github.com>
2023-05-11 01:34:58 +0400

    v11.05.2023

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-
 database_path = 'database.sqlite3'
 admin_id = (1563866138, )
 hours_difference = 4  # Разница времени для логов (на хостинге другой часовой пояс)
 bot_token = "https://t.me/BotFather"
 weather_api_key = "https://home.openweathermap.org/users/sign_in"
 
-__version__ = "08.05.2023"
+__version__ = "11.05.2023"
 __autor__ = "EgorKhabarov"
 
 """
