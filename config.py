46d00b11faac4ce2f33108e56587eb2fa822dc7b
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2023-08-21 02:45:11 +0400

    v21.08.2023 Баг фиксы.

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ COMMANDS = (
 callbackTab = "⠀⠀⠀"  # Специальные прозрачные символы для заполнения
 
 
-__version__ = "20.08.2023"
+__version__ = "21.08.2023"
 __autor__ = "EgorKhabarov"
 
 bot_settings = """
