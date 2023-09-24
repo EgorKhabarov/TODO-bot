aba55c2bbe23ea7bde47c2c46c6c409a285aa113
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2023-09-25 02:17:39 +0400

    v25.09.2023

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
+import os
 from threading import Thread
 
 from flask import Flask
 
 
-Thread(target=lambda: __import__("os").system("python tgbot\main.py")).start()
+Thread(target=os.system, args=("python tgbot\main.py",)).start()
 
 app = Flask(__name__)
 
