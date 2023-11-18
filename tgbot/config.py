4f8b9e5a29d7450c18203ca7dc101e77d79c6338
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2023-11-19 03:37:09 +0400

    v19.11.2023 Добавлен модуль telegram_utils. Добавлена логика обработки перехода группы в супергруппу. Кнопки групп статусов будут в строках по две.

--- tgbot/config.py
+++ tgbot/config.py
@@ -45,7 +45,7 @@ COMMANDS = (
 callbackTab = "⠀⠀⠀"  # Специальные прозрачные символы для заполнения
 
 
-__version__ = "08.11.2023"
+__version__ = "19.11.2023"
 __autor__ = "EgorKhabarov"
 
 bot_settings = """
