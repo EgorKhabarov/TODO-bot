01b8f94e66618edd83f50baf0109cc10864d7344
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-01-06 03:37:42 +0400

    v06.01.2024 Исправление багов

--- todoapi/config.py
+++ todoapi/config.py
@@ -7,7 +7,6 @@ load_dotenv("todoapi/.env")
 
 DATABASE_PATH = os.getenv("DATABASE_PATH", r"data\database.sqlite3")
 VEDIS_PATH = os.getenv("VEDIS_PATH", r"data\export_cooldown.vedis")
-LOG_FILE = os.getenv("LOG_FILE", r"data\log.log")
 LOG_FILE = os.getenv("LOG_FILE", r"logs/latest.log")
 admin_id = (1563866138,)
 
