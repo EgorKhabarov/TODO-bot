377140856ce8a141a1e15fbdf2bf6ec743e22e5d
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-04-23 17:19:05 +0400

    v23.04.2024 Добавлен GITHUB_WEBHOOK

--- server.py
+++ server.py
@@ -23,6 +23,11 @@ def home():
     return "200", 200
 
 
+@app.route("/version")
+def version():
+    return config.__version__
+
+
 @app.route("/limit")
 def limit():
     if len(str(request.args)) > 200:
@@ -62,3 +67,89 @@ if (
     if bot_webhook_info.url != config.TELEGRAM_WEBHOOK_URL:
         bot.remove_webhook()
         bot.set_webhook(config.TELEGRAM_WEBHOOK_URL)
+
+
+if config.GITHUB_WEBHOOK and config.GITHUB_WEBHOOK_URL:
+    """
+    From https://habr.com/ru/articles/457348/
+    """
+    import git
+    import json
+    import hmac
+    import hashlib
+
+
+    def is_valid_signature(x_hub_signature, data, private_key):
+        # x_hub_signature and data are from the webhook payload
+        # private key is your webhook secret
+        hash_algorithm, github_signature = x_hub_signature.split("=", 1)
+        algorithm = hashlib.__dict__.get(hash_algorithm)
+        encoded_key = bytes(private_key, "latin-1")
+        mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+        return hmac.compare_digest(mac.hexdigest(), github_signature)
+
+    @app.route(config.GITHUB_WEBHOOK_URL, methods=["POST"])
+    def webhook():
+        if request.method != "POST":
+            return "OK"
+        else:
+            abort_code = 418
+            # Do initial validations on required headers
+            if "X-Github-Event" not in request.headers:
+                abort(abort_code)
+            if "X-Github-Delivery" not in request.headers:
+                abort(abort_code)
+            if "X-Hub-Signature" not in request.headers:
+                abort(abort_code)
+            if not request.is_json:
+                abort(abort_code)
+            if "User-Agent" not in request.headers:
+                abort(abort_code)
+
+            ua = request.headers.get("User-Agent")
+
+            if not ua.startswith("GitHub-Hookshot/"):
+                abort(abort_code)
+
+            event = request.headers.get("X-GitHub-Event")
+            if event == "ping":
+                return json.dumps({"msg": "Hi!"})
+            if event != "push":
+                return json.dumps({"msg": "Wrong event type"})
+
+            x_hub_signature = request.headers.get("X-Hub-Signature")
+            # webhook content type should be application/json for request.data to have the payload
+            # request.data is empty in case of x-www-form-urlencoded
+            if not is_valid_signature(
+                x_hub_signature,
+                request.data,
+                config.GITHUB_WEBHOOK_SECRET,
+            ):
+                print("Deploy signature failed: {sig}".format(sig=x_hub_signature))
+                abort(abort_code)
+
+            payload = request.get_json()
+            if payload is None:
+                print("Deploy payload is empty: {payload}".format(
+                    payload=payload))
+                abort(abort_code)
+
+            if payload["ref"] != "refs/heads/master":
+                return json.dumps({"msg": "Not master; ignoring"})
+
+            repo = git.Repo()
+            origin = repo.remotes.origin
+            pull_info = origin.pull()
+
+            if len(pull_info) == 0:
+                return json.dumps({"msg": "Didn't pull any information from remote!"})
+            if pull_info[0].flags > 128:
+                return json.dumps({"msg": "Didn't pull any information from remote!"})
+
+            commit_hash = pull_info[0].commit.hexsha
+            build_commit = f'build_commit = "{commit_hash}"'
+            print(f"{build_commit}")
+
+            os.system("touch {}".format(config.WSGI_PATH))
+
+            return "Updated PythonAnywhere server to commit {commit}".format(commit=commit_hash)
