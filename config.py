061c18b747a7e3ed04e0d6ef50130f10fa344c2b
EgorKhabarov <not.a.fan.of.broccoli@gmail.com>
2024-07-19 02:53:16 +0400

    v2024.07.19.2 small update

--- config.py
+++ config.py
@@ -76,5 +76,5 @@ Special transparent symbol for filling empty space in buttons
 "⠀" or chr(10240) or "\\U00002800"
 """
 
-__version__ = "2024.07.19.0"
+__version__ = "2024.07.19.2"
 __author__ = "EgorKhabarov"
